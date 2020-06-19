c56cc5aae76ab113ad1c80aa15460f3a66cc26a4
LizzieGuajardoMozo <lizziemgm97@gmail.com>
2020-06-19 12:41:35 -0500

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,25 @@
 class Api::V1::UsersController < ApplicationController
-    
+
     #Add action to controller GET /users/1
     def show
         render json: User.find(params[:id])
     end
+    
+    #POST /users
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: @user.errors, status: :unprocessable_entity end
+        end
+    private
+
+    #Only allow a trusted parameter "white list" through
+    def user_params 
+        params.require(:user).permit(:email, :password)
+    end
+
+    
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -16,6 +16,23 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
       # Test to ensure response contains the correct email
       json_response = JSON.parse(self.response.body)
       assert_equal @user.email, json_response['email']
-      
   end
+
+  #We check the creation of a user by sending a valid POST requets
+  #Then we check that an additional user exists in the database and that 
+  #the HTTP code of the responde is created (Status code 201)
+  test "should create user" do 
+    assert_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: 'test@test.org', password: '123456' } }, as: :json
+    end
+    assert_response :created 
+  end
+
+  #We check that the user is not created using and email already used
+  #HTTP code of the response is unprocessable_entitiy (status code 422)
+  test "should not create user with taken email" do 
+    assert_no_difference('User.count') do
+      post api_v1_users_url, params: { user: { email: @user .email, password: '123456' } }, as: :json
+  end
+  assert_response :unprocessable_entity end
 end
