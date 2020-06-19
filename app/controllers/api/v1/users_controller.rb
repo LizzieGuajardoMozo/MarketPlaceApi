e31308301199e4c7ad199be1f35bc13d11640412
LizzieGuajardoMozo <lizziemgm97@gmail.com>
2020-06-19 15:42:35 -0500

    Adds update action the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,25 +1,36 @@
 class Api::V1::UsersController < ApplicationController
-
-    #Add action to controller GET /users/1
+    before_action :set_user, only: %i[show update destroy]
+  
     def show
-        render json: User.find(params[:id])
+      render json: @user
     end
-    
-    #POST /users
+  
+    def update
+      if @user.update(user_params)
+        render json: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  
     def create
-        @user = User.new(user_params)
-
-        if @user.save
-            render json: @user, status: :created
-        else
-            render json: @user.errors, status: :unprocessable_entity end
-        end
-    private
-
-    #Only allow a trusted parameter "white list" through
-    def user_params 
-        params.require(:user).permit(:email, :password)
+      @user = User.new(user_params)
+  
+      if @user.save
+        render json: @user, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
 
-    
-end
+    private
+  
+    # Only allow a trusted parameter "white list" through.
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
+  end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,38 +1,40 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-
   setup do
     @user = users(:one)
   end
 
-  #Verifies acess to the User#show method
   test "should show user" do
-      get api_v1_user_url(@user), as: :json
-      assert_response :success
-      # Test to ensure response contains the correct email
-      json_response = JSON.parse(self.response.body)
-      assert_equal @user.email, json_response['email']
+    get api_v1_user_url(@user), as: :json
+    assert_response :success
+    # on teste que la réponse contient le courriel
+    json_response = JSON.parse(self.response.body)
+    assert_equal @user.email, json_response['email']
   end
 
-  #We check the creation of a user by sending a valid POST requets
-  #Then we check that an additional user exists in the database and that 
-  #the HTTP code of the responde is created (Status code 201)
-  test "should create user" do 
+  test "should create user" do
     assert_difference('User.count') do
       post api_v1_users_url, params: { user: { email: 'test@test.org', password: '123456' } }, as: :json
     end
-    assert_response :created 
+    assert_response :created
   end
 
-  #We check that the user is not created using and email already used
-  #HTTP code of the response is unprocessable_entitiy (status code 422)
-  test "should not create user with taken email" do 
+  test "should not create user with taken email" do
     assert_no_difference('User.count') do
-      post api_v1_users_url, params: { user: { email: @user .email, password: '123456' } }, as: :json
+      post api_v1_users_url, params: { user: { email: @user.email, password: '123456' } }, as: :json
+    end
+    assert_response :unprocessable_entity
   end
-  assert_response :unprocessable_entity end
+
+  test "should update user" do
+    patch api_v1_user_url(@user), params: { user: { email: @user.email, password: '123456' } }, as: :json
+    assert_response :success
+  end
+
+  test "should not update other user" do
+    patch api_v1_user_url(@user), params: { user: { email: 'bad_email', password: '123456' } }, as: :json
+    assert_response :unprocessable_entity
+  end
+  
 end
